20b306fa23e642b2b880f0b55de2997eab432a4c
Micro-hard <40321746+Micro-hard@users.noreply.github.com>
2020-06-15 00:28:27 +0800

    Update Y0613test.java

--- src/Ztest/Y0613test.java
+++ src/Ztest/Y0613test.java
@@ -2,8 +2,8 @@ package Ztest;
 
 public class Y0613test {
     public static int a=123;
-
     public static void main(String[] args) {
         System.out.println(a);
+        System.out.println("test001");
     }
 }
