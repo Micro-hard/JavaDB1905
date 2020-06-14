38c6819233f5f974fd084e30b460bbd746b15644
admin <admin@aliyun.com>
2020-06-15 00:43:21 +0800

    update test file

--- src/Ztest/Y0613test.java
+++ src/Ztest/Y0613test.java
@@ -4,6 +4,6 @@ public class Y0613test {
     public static int a=123;
     public static void main(String[] args) {
         System.out.println(a);
-        System.out.println("test1");
+        System.out.println("test");
     }
 }
